1572244ce4f5306c3dcb2df446369b85fedcf955
Justin Tran <justin.train101@gmail.com>
2020-07-30 17:46:19 -0400

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
+//import './routes.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+
+
+
+class App extends Component {
+  render() {
+    return ( 
+      <BrowserRouter>
+        <div>
+          <Navigation />
+            <Switch>
+             <Route path="/" component={Home} exact/>
+            <Route component={Error}/>
+           </Switch>
+        </div> 
+      </BrowserRouter>
+     
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+ 
+const home = () => {
+    return (
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Dolla
+          </p>
+        </header>
+        <button className="App-button1">Sign In</button>
+        <button className="App-button2">Sign Up</button>
+      </div>
+    );
+}
+ 
+export default home;
