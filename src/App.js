a9dd8a6339128c3cc567a961d79ef76c29ce7d90
sctcccellig <runsalot058@gmail.com>
2020-08-07 12:29:50 -0500

    signup in progress

--- src/App.js
+++ src/App.js
@@ -4,23 +4,38 @@ import SignIn from './components/SignIn'
 import {BrowserRouter, Route, Switch, Router} from 'react-router-dom';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
+import Contacts from './components/Contacts';
 import Home from './components/Home';
 import Error from './components/Error';
 import Navigation from './components/Navigation';
 import Budget from './components/Budget';
-import SignUp from './components/SignUp'
+import SignUp from './components/Signup/SignUp'
 
 
 
 class App extends Component {
+
+  // state = {
+  //   contacts: []
+  // }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:8080/user/9')
+  //   .then(res => res.json())
+  //   .then((data) => {
+  //     console.log(data)
+  //     this.setState({ contacts: data })
+  //   })
+  //   .catch(console.log)
+  // }
+
   render() {
     return ( 
+      
       <BrowserRouter>
         <div>
-          <Navigation />
-          
-              <Switch>
+          <Navigation></Navigation>
+          <Switch>
               <Route path="/" component={Home} exact/>
               <Route path="/Budget" component={Budget} exact/>
                 <div className="auth-wrapper">
@@ -30,7 +45,7 @@ class App extends Component {
                   </div>
                 </div>
               <Route component={Error}/>
-              </Switch>
+          </Switch>
             
         </div> 
       </BrowserRouter>
---
+++ src/components/Signup/SignUp.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import { NavLink } from 'react-router-dom';
+
+// export interface Props {
+//     signUpAuthenication: Function;
+//     loggedIn: ()=>void
+// }
+
+// export interface State {
+//     username: string;
+//     email?: string;
+//     password: string;
+//     verifiedPassword: string;
+//     error: boolean;
+//     redirector: boolean;
+// }
+
+export default class SignUp extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {value: ''};
+    
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    //   }
+    
+    //   handleChange(event) {
+    //     this.setState({value: event.target.value});
+    //   }
+    
+    //   handleSubmit(event) {
+    //     alert('A name was submitted: ' + this.state.value);
+    //     event.preventDefault();
+    //   }
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstname: '',
+            lastname: '',
+            email: "",
+            password: '',
+            error: false,
+            redirector: false
+        };
+    }
+
+    handleSignUp = () =>{
+        //#TODO update url to meet api standard
+
+        fetch(`http://localhost:8080/user/`,{
+          method: 'post',
+            headers: {
+              'accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email: this.state.email, firstname: this.state.firstname,
+                 lastname: this.state.lastname, password: this.state.password})
+          })
+          .then((response)=>{
+            console.log(response)
+            //this.setState({redirector: true})
+            //this.props.loggedIn()
+          })
+          .catch((err)=>{
+            console.log(err)
+          })
+      }
+
+    render() {
+        return (
+            <form>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" id="firstname" name="firstname"
+                     placeholder="First name" onChange={(e) => this.setState({ firstname : e.target.value})}
+                     required />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" id="lastname" name="lastname"
+                     placeholder="Last name"  onChange={(e) => this.setState({ lastname : e.target.value})}
+                     required/>
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" id="email" name="email"
+                     placeholder="Enter email" onChange={(e) => this.setState({ email : e.target.value})}
+                     required/>
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" id="password" name="password"
+                     placeholder="Enter password" onChange={(e) => this.setState({ password : e.target.value})}
+                    required/>
+                </div>
+
+                <button id="submitButton" type="submit" className="btn btn-primary btn-block" value="submit" onClick={() => {
+                this.handleSignUp()}}>Sign Up</button>
+
+                <p className="forgot-password text-right">
+                    Already registered? <NavLink to="/SignIn">Sign In</NavLink>
+                </p>
+            </form>
+        );
+    }
+}
